7823f161fbd0b1159e7b4490f69402f0f9549eed
ajayj <ajayj@logicmatter.com>
2017-10-01 23:02:26 +0530

    ngClass Example

--- Angular2App/src/app/app.component.ts
+++ Angular2App/src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template:`<button class="colorClass" [class]='classesToApply'>My Button</button>`
+  template:`<button class="colorClass" [class]='classesToApply'>My Button</button>
+  <br/><br/>
+  <button class="colorClass" [class.boldClass]='applyBoldClass'>My Button</button>
+  <button class="colorClass" [class.boldClass]='!applyBoldClass'>Negation condition Button</button>
+  <br/>
+  <br/>
+  <button class="colorClass" [ngClass]='addClasses()'>NgClass  Button</button>`
   //templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
   classesToApply: string = 'italicClass boldClass';
+  applyBoldClass : boolean = true;
+  applyItalicClass : boolean = true;
+
+  addClasses(){
+    let classes = {
+        boldClass : this.applyBoldClass,
+        italicClass:this.applyItalicClass
+    };
+    return classes;
+  }
 }
