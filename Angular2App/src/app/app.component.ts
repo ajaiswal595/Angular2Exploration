ed3945c108f410c312c88ef36c52d93dc1eb61e9
ajayj <ajayj@logicmatter.com>
2017-10-01 22:47:46 +0530

    class Binding

--- Angular2App/src/app/app.component.ts
+++ Angular2App/src/app/app.component.ts
@@ -2,18 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template:`<button class="colorClass">My Button</button>`
+  template:`<button class="colorClass" [class]='classesToApply'>My Button</button>`
   //templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
-  //name : string = 'Angular 2!';
-  pageHeader: string ="Employee Details";
-  imagePath: string ="https://avatars2.githubusercontent.com/u/8745632?v=4&s=460";
-  isDisabled : boolean=false;
-  firstName : string ='Ajay';
-  lastName : string = 'Jaiswal';
-
-  getFullName():string{
-    return this.firstName+" "+this.lastName
-  }
+  classesToApply: string = 'italicClass boldClass';
 }
