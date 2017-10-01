b216840e3253b203cc9dd43a4c20b20d0106b899
ajayj <ajayj@logicmatter.com>
2017-10-01 22:42:04 +0530

    Added class property  for button.

--- Angular2App/src/app/app.component.ts
+++ Angular2App/src/app/app.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template:`<div>
-                <h1>{{'Page header = '+pageHeader}}</h1>
-                <img src={{imagePath}}/>
-                <h4>{{getFullName()}}</h4>
-                <my-employee></my-employee>
-                <button [disabled]='isDisabled'>Click Me</button>
-                <button bind-disabled='isDisabled'>Click Me Canonical</button>
-            </div>`
+  template:`<button class="colorClass">My Button</button>`
   //templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
