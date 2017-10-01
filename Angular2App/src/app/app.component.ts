36dd2dea70bf5f7177a393c668ac4dfa57e25f99
ajayj <ajayj@logicmatter.com>
2017-10-01 22:29:06 +0530

    interpolation and binding

--- Angular2App/src/app/app.component.ts
+++ Angular2App/src/app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template:`<div>
+                <h1>{{'Page header = '+pageHeader}}</h1>
+                <img src={{imagePath}}/>
+                <h4>{{getFullName()}}</h4>
+                <my-employee></my-employee>
+                <button [disabled]='isDisabled'>Click Me</button>
+                <button bind-disabled='isDisabled'>Click Me Canonical</button>
+            </div>`
+  //templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  //name : string = 'Angular 2!';
+  pageHeader: string ="Employee Details";
+  imagePath: string ="https://avatars2.githubusercontent.com/u/8745632?v=4&s=460";
+  isDisabled : boolean=false;
+  firstName : string ='Ajay';
+  lastName : string = 'Jaiswal';
+
+  getFullName():string{
+    return this.firstName+" "+this.lastName
+  }
+}
