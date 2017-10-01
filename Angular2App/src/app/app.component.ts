160932d86d143adfb25877d5f57f965b9fe831f0
ajayj <ajayj@logicmatter.com>
2017-10-01 23:35:12 +0530

    event binding

--- Angular2App/src/app/app.component.ts
+++ Angular2App/src/app/app.component.ts
@@ -2,25 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template:`<button style='color:red' [style.font-weight]="isBold ? 'bold':'normal'">My Button</button>
-  <br/>
-  <button style='color:red' [style.font-weight]="isBold ? 'bold':'normal'" [style.font-size.px]="fontSize">My Button</button>
-  <br/><br/>
-  <button style='color:red' [ngStyle]="addStyles()">My Button3</button>`
-  //templateUrl: 'app/app.component.html',
+  template:`<button (click)='onClick()'>My onClick event</button>
+  <br/><button on-click='onClick()'>canonicle binding</button>`
 })
 export class AppComponent {
-  isBold : boolean = true;
-  fontSize : number = 30;
-  isItalic : boolean = true;
-
-  addStyles(){
-    let styles = {
-      'font-size.px':this.fontSize,
-      'font-style':this.isItalic ?'italic' :'normal',
-      'font-weight':this.isBold?'bold':'normal'
-    };
-    return styles;
-    
-  }
+ onClick():void{
+   console.log('ButtonClicked');
+ }
 }
