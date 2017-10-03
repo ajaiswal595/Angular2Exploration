295d15310ff0b35421b8457e6f0c079dc770bfd0
ajayj <ajayj@logicmatter.com>
2017-10-03 14:36:42 +0530

    trackBy

--- Angular2App/src/app/employee/employeeList.components.ts
+++ Angular2App/src/app/employee/employeeList.components.ts
@@ -5,10 +5,27 @@ import { Component } from '@angular/core';
     styleUrls: ['app/employee/employeeList.components.css']
 })
 export class EmployeeListComponent{
-    employees: any[]=[
-        {code:'emp01',name:'tom',gender:'male',anualSalary:5500,dateOfBirth:'20-may-1992' },
-        {code:'emp02',name:'ajay',gender:'male',anualSalary:32000,dateOfBirth:'20-may-1992'},
-        {code:'emp03',name:'Lovely',gender:'female',anualSalary:5500,dateOfBirth:'31-dec-1992'},
-        {code:'emp04',name:'Shreya',gender:'female',anualSalary:64533,dateOfBirth:'12-sep-1988'}
-    ];
+    employees:any[];
+    constructor(){
+        this.employees=[
+            {code:'emp01',name:'tom',gender:'male',anualSalary:5500,dateOfBirth:'20-may-1992' },
+            {code:'emp02',name:'ajay',gender:'male',anualSalary:32000,dateOfBirth:'20-may-1992'},
+            {code:'emp03',name:'Lovely',gender:'female',anualSalary:5500,dateOfBirth:'31-dec-1992'},
+            {code:'emp04',name:'Shreya',gender:'female',anualSalary:64533,dateOfBirth:'12-sep-1988'}
+        ];
+    }
+    
+    getEmployees():void {
+        this.employees=[
+            {code:'emp01',name:'tom',gender:'male',anualSalary:5500,dateOfBirth:'20-may-1992' },
+            {code:'emp02',name:'ajay',gender:'male',anualSalary:32000,dateOfBirth:'20-may-1992'},
+            {code:'emp03',name:'Lovely',gender:'female',anualSalary:5500,dateOfBirth:'31-dec-1992'},
+            {code:'emp04',name:'Shreya',gender:'female',anualSalary:64533,dateOfBirth:'12-sep-1988'},
+            {code:'emp05',name:'NewAdded',gender:'female',anualSalary:99999,dateOfBirth:'19-sep-1999'}
+        ];
+    }
+
+    trackByEmpCode(index:number , employee:any):string{
+        return employee.code;
+    }
 }
