ba7a65fec832e983875d48b816d8fa21b3a39c76
Ajay <ajaiswal595@gmail.com>
2018-03-18 22:45:26 +0530

    favorite. glyphicon

--- Angular2App/src/app/app.module.ts
+++ Angular2App/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
-import {SummaryPipe } from './summary.pipe'
+import {SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {SummaryPipe } from './summary.pipe'
     CoursesComponent,
     CourseComponent,
     AuthorsComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,FormsModule
